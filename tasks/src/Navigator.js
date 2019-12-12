8f262047d34b1cced4889e794d9666fa58177a95
Thiago Pereira <thiagopereiramail@gmail.com>
2019-12-12 10:07:24 -0300

    Configurando a navegação entre telas no react-native.

--- tasks/src/Navigator.js
+++ tasks/src/Navigator.js
@@ -1,28 +1,10 @@
-// import React from 'react';
-import { createAppContainer, createStackNavigator } from 'react-navigation';
+import { createSwitchNavigator, createAppContainer } from 'react-navigation';
 import Agenda from './screens/Agenda';
 import Auth from './screens/Auth';
 
-// const MainRoutes = {
-//     Auth: {
-//         name: 'Auth',
-//         screen: Auth,
-//     },
-//     Home: {
-//         name: 'Home',
-//         screen: Agenda,
-//     },
-// }
+const mainNavigation = createSwitchNavigator({
+  Auth,
+  Agenda,
+})
 
-// const MainNavigator = createSwitchNavigator(MainRoutes, { initialRouteName: 'Auth' })
-
-// export default MainNavigator;
-
-const Navigator = createAppContainer(
-    createStackNavigator({
-      Home: Agenda,
-      Auth,
-    })
-)
-
-export default Navigator;
+export default createAppContainer(mainNavigation);
