e352b250920faf814da40da94abf29be39682657
Thiago Pereira <thiagopereiramail@gmail.com>
2019-12-16 14:56:56 -0300

    Configurando o react-navigation para a versão utilizada no curso.

--- tasks/src/Navigator.js
+++ tasks/src/Navigator.js
@@ -1,10 +1,67 @@
-import { createSwitchNavigator, createAppContainer } from 'react-navigation';
+import React from 'react';
+import { createSwitchNavigator, createDrawerNavigator } from 'react-navigation';
 import Agenda from './screens/Agenda';
 import Auth from './screens/Auth';
+import commonStyles from './commonStyles';
 
-const mainNavigation = createSwitchNavigator({
-  Auth,
-  Agenda,
-})
+const menuRoutes = {
+    Today: {
+        name: 'Today',
+        screen: props => <Agenda title='Hoje' daysAhead={0} {...props} />,
+        navigationOptions: {
+            title: 'Hoje',
+        },
+    },
+    Tomorrow: {
+        name: 'Tomorrow',
+        screen: props => <Agenda title='Amanhã' daysAhead={1} {...props} />,
+        navigationOptions: {
+            title: 'Amanhã',
+        },
+    },
+    Week: {
+        name: 'Week',
+        screen: props => <Agenda title='Semana' daysAhead={7} {...props} />,
+        navigationOptions: {
+            title: 'Semana',
+        },
+    },
+    Month: {
+        name: 'Month',
+        screen: props => <Agenda title='Mês' daysAhead={30} {...props} />,
+        navigationOptions: {
+            title: 'Mês',
+        },
+    },
+}
 
-export default createAppContainer(mainNavigation);
+const menuConfig = {
+    initialRouteName: 'Today',
+    contentOptions: {
+        labelStyle: {
+            fontFamily: commonStyles.fontFamily,
+            fontWeight: 'normal',
+            fontSize: 20,
+        },
+        activeLabelStyle: {
+            color: '#080',
+        }
+    },
+}
+
+const menuNavigator = createDrawerNavigator(menuRoutes, menuConfig);
+
+const mainRoutes = {
+    Auth: {
+        name: 'Auth',
+        screen: Auth,
+    },
+    Home: {
+        name: 'Home',
+        screen: menuNavigator,
+    },
+}
+
+const mainNavegator = createSwitchNavigator(mainRoutes, { initialRouteName: 'Auth' });
+
+export default mainNavegator;
